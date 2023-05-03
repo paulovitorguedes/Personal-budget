ad71dbbfd3b75576ff6b027f6dd267726d33d6ea
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2023-05-03 00:16:24 -0300

    Function cadastrar
    atribuir evento onclick ao icon plus para função cadastrar

---
+++ _js/script.js
@@ -0,0 +1,11 @@
+
+
+
+function cadastrar() {
+    let ano = document.getElementById('ano');
+    let mes = document.getElementById('mes');
+    let dia = document.getElementById('dia');
+    let tipo = document.getElementById('tipo');
+    let descricao = document.getElementById('descricao');
+    let valor = document.getElementById('valor');
+}
